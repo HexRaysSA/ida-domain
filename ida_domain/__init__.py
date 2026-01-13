951f2bca16d3b7252f46035c785f13fd923d9f2c
semantic-release <semantic-release>
2026-01-13 14:08:15 +0000

    0.3.6-dev.3
    Automatically generated by python-semantic-release

--- ida_domain/__init__.py
+++ ida_domain/__init__.py
@@ -25,7 +25,7 @@ def _load_dependencies() -> None:
         import idapro
 
 
-__version__ = '0.3.6-dev.2'
+__version__ = '0.3.6-dev.3'
 
 # Make sure all dependencies are loaded
 _load_dependencies()
